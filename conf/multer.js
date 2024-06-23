9cd258ea7d2231f968072081267ebab101d9c94c
Samuel Luna <92549469+gsgluna@users.noreply.github.com>
2024-06-22 22:52:33 -0300

    Update multer.js

--- conf/multer.js
+++ conf/multer.js
@@ -1,1 +1,30 @@
 
+
+const multer = require('multer');
+const path = require('path');
+const aws = require('aws-sdk');
+const multerS3 = require('multer-s3');
+  
+aws.config.update({
+  accessKeyId: process.env.ACCESSKEY,
+  secretAccessKey: process.env.SECRETACCESSKEY,
+  region: "us-east-1",
+
+});
+
+const s3 = new aws.S3();
+const upload = multer({ 
+  storage: multerS3({
+
+    s3: s3,
+    bucket: "immuniecarteirinha",
+    acl: "public-read",
+    key(req,file,cb){
+      cb(null, 'User_Image' + path.extname(file.originalname));
+    }
+
+  })
+
+ });
+ 
+module.exports = upload;
